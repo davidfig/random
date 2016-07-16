2d526a6d91bf8794d6f93c446517adc6f10f77e7
David Figatner <david@yopeyopey.com>
2016-07-16 11:20:13 -0400

    Started work on xor random numbers

--- random.js
+++ random.js
@@ -19,6 +19,71 @@ function seed(seed)
     };
 }
 
+/*
+// changes the generator to use a seeded random function based on XOR
+// see http://baagoe.com/en/RandomMusings/javascript/ (https://web.archive.org/web/20101106000458/http://baagoe.com/en/RandomMusings/javascript/)
+// as seen here: http://jsdo.it/akm2/amk0
+function seedXORshift(seed)
+{
+    function mash(data)
+    {
+        data = data.toString();
+        var n = 0xefc8249d;
+        for (var i = 0; i < data.length; i++)
+        {
+            n += data.charCodeAt(i);
+            var h = 0.02519603282416938 * n;
+            n = h >>> 0;
+            h -= n;
+            h *= n;
+            n = h >>> 0;
+            h -= n;
+            n += h * 0x100000000;
+        }
+        return (n >>> 0) * 2.3283064365386963e-10;
+    }
+
+    generator = function()
+    {
+        var self = this;
+        var seeds = (arguments.length) ? Array.prototype.slice.call(arguments) : [new Date().getTime()];
+
+        var x = 123456789;
+        var y = 362436069;
+        var z = 521288629;
+        var w = 88675123;
+        var v = 886756453;
+
+        self.uint32 = function()
+        {
+            var t = (x ^ (x >>> 7)) >>> 0;
+            x = y;
+            y = z;
+            z = w;
+            w = v;
+            v = (v ^ (v << 6)) ^ (t ^ (t << 13)) >>> 0;
+            return ((y + y + 1) * v) >>> 0;
+        };
+
+        self.random = function() {
+            return self.uint32() * 2.3283064365386963e-10;
+        };
+
+        self.fract53 = function() {
+            return self.random() + (self.uint32() & 0x1fffff) * 1.1102230246251565e-16;
+        };
+
+        for (var i = 0, len = seeds.length, seed; i < len; i++) {
+            seed = seeds[i];
+            x ^= mash(seed) * 0x100000000;
+            y ^= mash(seed) * 0x100000000;
+            z ^= mash(seed) * 0x100000000;
+            v ^= mash(seed) * 0x100000000;
+            w ^= mash(seed) * 0x100000000;
+        }
+}
+*/
+
 // resets the random number generator to Math.random()
 function reset()
 {
